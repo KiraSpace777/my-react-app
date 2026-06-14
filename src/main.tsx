f7dd1ff4d02ed0cb1c8ddee42d4330afd7139044
Anna.K <flysnow@ukr.net>
2026-06-15 01:37:18 +0300

    Workbook notes 3.2 (JSX)

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,42 @@
 // ----------------------------------------------
-// --------- "ZERO"/install CODE ----------------
+// ---------- Getting start CODE ----------------
 // ----------------------------------------------
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 );
 
+// ----------------------------------------------
+// --------- Workbook notes 3.2 coding ----------
+// ----------------------------------------------
+
+// import { createRoot } from "react-dom/client";
+
+// const techName = "React";
+// const imgUrl =
+//   "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?w=640";
+
+// createRoot(document.getElementById("root") as HTMLElement).render(
+//   <>
+//     <h1>Welcome to {techName}</h1>
+//     <p>
+//       This is JSX — it looks like HTML, but it's not quite the same. It has its
+//       // own rules! JSX (JavaScript XML) is a special syntax in React that
+//       allows // you to write HTML-like markup directly in JavaScript or
+//       TypeScript files. // Instead of creating a separate .html file, in React
+//       you describe the // interface directly in the code, using JSX.
+//     </p>
+//     <img src={imgUrl} alt="Man, field and a mountain" width="640" />
+//   </>,
+// );
+
+// ----------------------------------------------
 // --------- Workbook notes 3.1 coding ----------
 // ----------------------------------------------
 
@@ -21,10 +46,25 @@ createRoot(document.getElementById("root")!).render(
 //   <div>
 //     <h1>Welcome to React</h1>
 //     <p>
-//       This is JSX — it looks like HTML, but it's not quite the same. It has its own rules!
-//     </p>
-//     <p>
-//       JSX (JavaScript XML) is a special syntax in React that allows you to write HTML-like markup directly in JavaScript or TypeScript files. Instead of creating a separate .html file, in React you describe the interface directly in the code, using JSX.
+//       This is JSX — it looks like HTML, but it's not quite the same. It has its
+//       own rules! JSX (JavaScript XML) is a special syntax in React that allows
+//       you to write HTML-like markup directly in JavaScript or TypeScript files.
+//       Instead of creating a separate .html file, in React you describe the
+//       interface directly in the code, using JSX.
 //     </p>
 //   </div>,
+// );
+
+// ----------------------------------------------
+// --------- "ZERO"/install CODE ----------------
+// ----------------------------------------------
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App.tsx";
+
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
 // );
